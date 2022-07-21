41e16af30334e683d0f8e24c8164a8e6c3035f5a
andrewhoward96 <99508807+andrewhoward96@users.noreply.github.com>
2022-07-21 14:50:35 +0100

    update test

--- src/test/java/hotelTest/RoomTest.java
+++ src/test/java/hotelTest/RoomTest.java
@@ -1,4 +1,12 @@
-package hotelTest;
+import org.junit.Before;
 
 public class RoomTest {
+
+    Room room;
+
+    @Before
+    public void before() {
+        room = new Room(RoomType.SINGLE);
+    }
+
 }
